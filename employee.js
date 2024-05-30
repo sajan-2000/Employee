e0517db91617b1302c21f7fe38fecd9575e5386c
Sajan Kumar Swain <sajankumarswain1@gmail.com>
2024-05-30 13:22:13 +0530

    employee assignment

---
+++ employee.js
@@ -0,0 +1,31 @@
+let employeeList = [{
+    name: "Sajan",
+    id: 1,
+    attendance: ""
+},
+{
+    name: "Salman",
+    id: 2,
+    attendance: ""
+
+},
+{
+    name: "Shahruk",
+    id: 3,
+    attendance: ""
+
+},
+{
+    name: "Katrina",
+    id: 4,
+    attendance: ""
+
+},
+{
+    name: "Kiara",
+    id: 5,
+    attendance: ""
+
+}
+]
+
